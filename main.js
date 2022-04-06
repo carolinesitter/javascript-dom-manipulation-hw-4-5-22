87f56d35995107171a347740434b0ba66606660f
Caroline <carolinesitter24@gmail.com>
2022-04-05 19:29:34 -0500

    colorChange Function Completed

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// create a function colorChange
+function colorChange() {
+    // make variable that pulls out each class id color-change
+    const makeRedElements = document.querySelectorAll('.color-change');
+
+    // loop through each element and change to red
+    for (const element of makeRedElements){
+        element.classList.add('red');
+    }
+}
+
